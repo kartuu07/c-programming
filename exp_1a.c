cf7c2002b29dc3a65b48ffd6a07c8e1767805080
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2025-10-06 16:08:22 +0530

    Add files via upload

---
+++ exp_1a.c
@@ -0,0 +1,13 @@
+// Name: Kartvya Bhoir
+//UIN: 251C034 //
+// Branch: Civil //
+// Div: B //
+#include <stdio.h>
+int main(){
+printf("Name: Kartvya Bhoir");
+printf("\nAge: 18");
+printf("\nClass: Engeneering 1st year");
+printf("\nBranch: Civil");
+printf("\nUIN: 251C034");
+return 0;
+}
