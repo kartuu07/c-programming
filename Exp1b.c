c926341af1af4865d998ca6188770677feb9081b
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2025-10-08 19:31:25 +0530

    Exp1b.c

--- Exp1b.c
+++ Exp1b.c
@@ -16,11 +16,6 @@ printf("Maths marks: ");
 scanf("%d",&maths_mark);
 float average = (physics_mark+chemistry_mark +maths_mark)/3.0;
 printf("\nAverage marks: %f", average);
-if (50<=average && 100>=average) {
-printf("\nYou are eligible");
-} else if (average>100) {
-printf("\nError٢٢٢٢٢٢٢");
-} else
-printf("\nYou are not eligible");
+(50<=average)? printf("You are eligible"); (50>average) printf("You are not eligible");
 return 0;
 }
