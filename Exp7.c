9a06736276c131cdca7d7285d5d4af0c0c7e3684
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2025-11-20 11:58:19 +0530

    Add files via upload

---
+++ Exp7.c
@@ -0,0 +1,31 @@
+/*Name; Kartvya bhoir
+Roll no. 25
+UIN: 251C034
+Branch; Civil*/
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element in the array is: %d\n", largest);
+
+    return 0;
+}
