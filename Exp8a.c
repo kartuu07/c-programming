fcd5696eb18de844868756e93e44141df0608796
Kartvya bhoir <kartvyabhoir2007@gmail.com>
2025-11-20 12:26:18 +0530

    Add files via upload

---
+++ Exp8a.c
@@ -0,0 +1,21 @@
+/*Name; Kartvya bhoir
+Roll no. 25
+UIN: 251C034
+Branch; Civil*/
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  
+    
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    printf("Length of the string = %d\n", length);
+
+    return 0;
+}
